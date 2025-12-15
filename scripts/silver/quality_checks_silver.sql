692fcb50f6327d5e75256d1d1568eb606d1a0ced
Robinson Tamilselvan <100284379+trobinuk@users.noreply.github.com>
2025-12-15 14:24:42 -0600

    Update quality_checks_silver.sql

--- scripts/silver/quality_checks_silver.sql
+++ scripts/silver/quality_checks_silver.sql
@@ -5,6 +5,12 @@ Purpose:
 --Check for Nulls or Duplicates in Primary key
 --Expectation: No Result
 
+------------------------------------------------------------------------------------------------------------------
+------------------------------------------silver.crm_cust_info----------------------------------------------------
+------------------------------------------------------------------------------------------------------------------
+--Check for Nulls or Duplicates in Primary key
+--Expectation: No Result
+
 SELECT cst_id, COUNT(*)
 FROM silver.crm_cust_info
 GROUP BY cst_id
@@ -23,7 +29,7 @@ FROM silver.crm_cust_info
 WHERE cst_lastname != TRIM(cst_lastname);
 
 --Check for Data Standardization & Consistency
---Expectation: 3 without NUll
+--Expectation: 2 without NUll
 SELECT DISTINCT cst_marital_status
 FROM silver.crm_cust_info;
 
@@ -32,80 +38,54 @@ FROM silver.crm_cust_info;
 SELECT DISTINCT cst_gndr
 FROM silver.crm_cust_info;
 
-SELECT *
-FROM
-(SELECT ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) flag_last,*
-FROM silver.crm_cust_info) t
-WHERE t.flag_last = 1
-AND cst_gndr IS NULL;
-
-SELECT *
-FROM
-(SELECT ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) flag_last,*
-FROM silver.crm_cust_info) t
-WHERE t.flag_last = 1
-AND cst_marital_status IS NULL;
-
 --Check for unwanted spaces
 --Expectation: No Result
 SELECT cst_gndr
 FROM silver.crm_cust_info
 WHERE cst_gndr != TRIM(cst_gndr);
 
+----------------------------------------------------------------------------------------------------------------------
 ------------------------------------------silver.crm_prd_info----------------------------------------------------
-SELECT * FROM silver.crm_prd_info LIMIT 1000;
-
+-----------------------------------------------------------------------------------------------------------------
 --Check for Nulls or Duplicates in Primary key
 --Expectation: No Result
-
 SELECT prd_id, COUNT(*)
 FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL; --passed
 
+--Check for unwanted spaces
+--Expectation: No Result
 SELECT prd_key
 FROM silver.crm_prd_info
 WHERE prd_key != TRIM(prd_key); --passed
 
+--Check for unwanted spaces
+--Expectation: No Result
 SELECT prd_nm
 FROM silver.crm_prd_info
 WHERE prd_nm != TRIM(prd_nm); --passed
 
 --Check for Nulls or Negative (-ve)
 --Expectation: No Result
-
 SELECT *
 FROM silver.crm_prd_info
-WHERE prd_cost IS NULL OR  prd_cost < 0;
+WHERE prd_cost IS NULL OR prd_cost < 0;
 
---Check for distinct values (low cardinality) and null
+--Check for distinct values (low cardinality) and no null values
 --Expectation: No Result
 SELECT DISTINCT prd_line
 FROM silver.crm_prd_info;
---WHERE prd_line IS NULL;
 
 --Check if prd_start_dt > prd_end_date
 --Expectation: No Result
 SELECT *
 FROM silver.crm_prd_info
-WHERE  PRD_KEY = 'AC-HE-HL-U509-R'
---prd_start_dt >= prd_end_dt   AND
-LIMIT 1000;
---2007, 2008, 2011, 2012, 2013
+WHERE  prd_start_dt >= prd_end_dt ;
 
-SELECT *
-FROM silver.crm_prd_info
-WHERE  prd_start_dt >= prd_end_dt  
-
-SELECT *
-FROM silver.crm_prd_info
-WHERE  PRD_KEY = 'AC-HE-HL-U509-B'
-
-SELECT * FROM silver.crm_prd_info LIMIT 1000;
-
-
+----------------------------------------------------------------------------------------------------------------------
 ------------------------------------------silver.crm_sales_details----------------------------------------------------
-SELECT * FROM silver.crm_sales_details LIMIT 1000;
+----------------------------------------------------------------------------------------------------------------------
 
 --Check if all sls_prd_key exist in silver.crm_prd_info Table
 --Expectation: No Result
@@ -122,23 +102,18 @@ WHERE sls_cust_id NOT IN (SELECT DISTINCT cst_id  FROM silver.crm_cust_info);
 SELECT * FROM silver.crm_sales_details
 WHERE sls_sales <= 0 OR  sls_quantity <= 0 OR sls_price <= 0;
 
-SELECT * FROM silver.crm_sales_details
-WHERE sls_quantity <= 0 
+--Check if the date is valid (Greater than today)
+--Expectation: No Result
+SELECT distinct sls_order_dt FROM silver.crm_sales_details
+WHERE sls_order_dt < CAST('2010-01-01' AS Date) OR sls_order_dt > CURRENT_DATE;
 
 SELECT * FROM silver.crm_sales_details
-WHERE sls_sales <= 0
+WHERE sls_ship_dt < CAST('2010-01-01' AS Date) OR sls_order_dt > CURRENT_DATE;
 
 SELECT * FROM silver.crm_sales_details
-WHERE LENGTH(CAST(sls_order_dt AS VARCHAR(8))) = 8
-
-SELECT * FROM silver.crm_sales_details
-WHERE LENGTH(CAST(sls_ship_dt AS VARCHAR(8))) != 8
-
-SELECT * FROM silver.crm_sales_details
-WHERE LENGTH(CAST(sls_due_dt AS VARCHAR(8))) != 8
+WHERE sls_due_dt < CAST('2010-01-01' AS Date) OR sls_order_dt > CURRENT_DATE;
 
 ------------------------------------------silver.erp_cust_az12----------------------------------------------------
-SELECT * FROM silver.erp_cust_az12 LIMIT 1000;
 
 SELECT UPPER(TRIM(gen)) FROM silver.erp_cust_az12
 WHERE gen != TRIM(gen);
@@ -154,7 +129,6 @@ SELECT COUNT(*) FROM silver.erp_cust_az12; --18484
 
 SELECT * FROM silver.erp_cust_az12;
 ---------------------------------------------------silver.erp_loc_a101-----------------------------
-SELECT * FROM silver.erp_loc_a101 LIMIT 1000; --18484
 
 SELECT * FROM silver.erp_loc_a101 
 WHERE cid NOT LIKE 'AW-%';
@@ -180,7 +154,6 @@ SELECT REPLACE(cid,'-','') cid,
 		ELSE TRIM(cntry) END cntry
 FROM silver.erp_loc_a101;
 ---------------------------------erp_px_cat_g1v2-----------------
-SELECT * FROM silver.erp_px_cat_g1v2 LIMIT 1000;
 
 SELECT DISTINCT subcat
 FROM silver.erp_px_cat_g1v2;
@@ -200,7 +173,7 @@ SELECT * FROM silver.erp_px_cat_g1v2
 WHERE REPLACE(id,'_','-') NOT IN (SELECT DISTINCT cat_id FROM silver.crm_prd_info);
 
 SELECT DISTINCT cat_id FROM silver.crm_prd_info
-ORDER BY cat_id
+ORDER BY cat_id;
 
 
 
